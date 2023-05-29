1bdd04d107c2805b1d2274efd298cac09da87d24
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-05-29 18:34:18 +0200

    setting up testing with pytest

---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+from config import config, Config
+# from app.service_integration_api import Pihole_Consumer
---
+++ tests/test_pihole_integration.py
@@ -0,0 +1,25 @@
+import pytest
+from app.service_integration_api import PiholeConsumer
+import requests
+from tests import utils
+
+
+def test_get_all_queries(mocker):
+    pihole_consumer = PiholeConsumer("test_auth_token")
+
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"key": "value"}
+
+    mocker.patch('requests.get', return_value=mock_response)
+
+    from_datetime, until_datetime = utils.generate_random_from_until_datetimes()
+    from_str = utils.datetime_to_string(from_datetime)
+    until_str = utils.datetime_to_string(until_datetime)
+
+    result = pihole_consumer.get_all_queries(
+        from_str, until_str)
+
+    requests.get.assert_called_once_with(
+        "http://pi.hole/admin/api.php", params={"auth": "test_auth_token", "getAllQueries": "1", "from": str(utils.datetime_to_unix_timestamp(from_datetime)), "until": str(utils.datetime_to_unix_timestamp(until_datetime))})
+    assert result == {"key": "value"}
