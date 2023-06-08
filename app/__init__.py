84a35d53af7c03e8093982ff68d43fc6295871b0
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-06-08 12:02:20 +0200

    Change from APScheduler to CLI job

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,21 @@
 # Bundle all sections and expose the Flask APP
 from flask import Flask
 from config import Config
-from app.monitors.pihole_monitor import fetch_query_data_job
-from app.extensions import scheduler
 import logging
 
 
 def create_app(config: Config):
-    appp = Flask(__name__)
-    appp.config.from_object(config)
+    app = Flask(__name__)
+    app.logger.info('creating app...')
+    app.config.from_object(config)
 
-    appp.logger.setLevel(logging.INFO)
+    app.logger.setLevel(logging.INFO)
 
-    appp.logger.info('starting app...')
-    scheduler.init_app(appp)
-    logging.getLogger("apscheduler").setLevel(logging.DEBUG)
-
-    with appp.app_context():
-        scheduler.add_job(id='pihole_monitor', func=fetch_query_data_job,
-                          trigger='interval', seconds=appp.config['SCHEDULER_TIMEINTERVAL'])
-        scheduler.start()
-
-    # with appp.app_context():
-     #   fetch_query_data_job()
+    from app.extensions import db
+    db.init_app(app)
 
     from app import views
+    app.register_blueprint(views.bp)
 
-    appp.register_blueprint(views.bp)
+    return app
 
-    return appp
-
-# db_uri = app.config['SQLALCHEMY_DATABASE_URI']
-# influxdb_host = app.config['INFLUXDB_HOST']
-
-# move to model (see patterns in documentation) use current_app
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
