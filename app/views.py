bd1f14076fc2dfe1163680bfa985687379d0c583
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-06-15 14:46:38 +0200

    setting up UI with navigation and devices page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 # App routing
-from flask import Blueprint, redirect
+from flask import Blueprint, render_template
 
-bp = Blueprint("main", __name__)
+bp = Blueprint("main", __name__, template_folder="templates")
 
 
 @bp.route("/")
 def index():
-    return redirect("http://grafana:3000/")
+    return render_template("index.html")
+
+
+@bp.route("/devices")
+def devices():
+    return render_template("devices.html")
