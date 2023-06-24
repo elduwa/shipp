15a158add8937513200b0d634173f6b919dee2ad
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-06-24 19:59:22 +0200

    linting

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField
 from wtforms.validators import DataRequired, IPAddress, MacAddress
 
 
