57aeea5a5b189922104a7282bc411cf57cb4de50
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-07-02 22:43:28 +0200

    fix nginx setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 # Bundle all sections and expose the Flask APP
 from flask import Flask
-from config import Config
+from config import config
 import logging
 from flask.logging import default_handler
 
 
-def create_app(config: Config):
+def create_app(config_name: str):
     app = Flask(__name__)
-    app.config.from_object(config)
-    config.init_app(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    if config_name == "production":
+        from werkzeug.middleware.proxy_fix import ProxyFix
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
 
     with app.app_context():
         app.logger.setLevel(logging.INFO)
