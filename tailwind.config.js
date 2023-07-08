115d8eaa8e78f86229741809da83d60e58d5a957
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-07-08 17:39:05 +0200

    minor bg color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            "base-french-gray": "#C0CCD87C",
+            "base-french-gray": "#C0CCD844",
             "accent-1-blue-crayola": "#687bf3ff",
             "accent-2-space-cadet": "#2d3047ff",
             "text-1-davys-grey": "#525251ff"
