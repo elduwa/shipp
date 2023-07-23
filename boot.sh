89ec792ab7be4d740cd30e6c02d6b6916d841d50
Elliott Wallace <elliott.d.wallace@gmail.com>
2023-07-23 17:42:56 +0200

    try new boot.sh to create db if not exists

--- boot.sh
+++ boot.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+
 source .venv/bin/activate
 
-echo "Migrating database..."
-while true; do
-    flask deploy
-    if [ "$?" -eq "0" ]; then
-        break
-    fi
-    echo Deploy command failed, retrying in 5 secs...
-    sleep 5
-done
-echo "Database migrated"
+database_file="/opt/webapp/data/rel_db/sqlite.db"
+
+if [ -f "$database_file" ]; then
+    echo "Running deploy routine..."
+    while true; do
+        flask deploy
+        if [ "$?" -eq "0" ]; then
+            break
+        fi
+        echo "Deploy command failed, retrying in 5 secs..."
+        sleep 5
+    done
+    echo "Database migrated"
+else
+    echo "Creating database..."
+    flask db create-all
+    echo "Database created"
+fi
 
 echo "Starting cron..."
 cron
-echo "cron started"
+echo "Cron started"
 
 echo "Starting gunicorn..."
 exec gunicorn -b :8000 --access-logfile - --error-logfile - run:app
-
